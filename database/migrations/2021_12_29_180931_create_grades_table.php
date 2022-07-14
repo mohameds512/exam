1dfae8178f45461e8a9a23375bc802b312cbbe46
unknown <mhmde6373@gmail.com>
2022-07-14 18:27:53 +0200

    fst Update

--- database/migrations/2021_12_29_180931_create_grades_table.php
+++ database/migrations/2021_12_29_180931_create_grades_table.php
@@ -15,7 +15,7 @@ class CreateGradesTable extends Migration
     {
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
-            $table->json('name')->unique();
+            $table->text('name')->unique();
             $table->text('notes')->nullable();
             $table->timestamps();
         });
