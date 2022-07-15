13c3aed144a8ea6a3cb0cf5dcfb87051492ade21
unknown <mhmde6373@gmail.com>
2022-07-15 17:47:13 +0200

    bio edit

--- database/migrations/2021_12_10_144845_add_bio_to_users.php
+++ database/migrations/2021_12_10_144845_add_bio_to_users.php
@@ -14,7 +14,7 @@ class AddBioToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('bio')->null();
+            $table->text('bio')->nullable();
         });
     }
 
